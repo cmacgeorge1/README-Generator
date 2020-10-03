081275ef7bf552cf0e220d2862092908b3cbc9da
Chris MacGeorge <christopher.s.macgeorge@gmail.com>
2020-10-02 22:38:29 -0700

    added js file with outline

---
+++ index.js
@@ -0,0 +1,14 @@
+// Require dependancies
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+// Create an Array of questions
+
+
+// Write a README in a markdown file as a template
+
+
+// Function that will generate my README template
+
+
+// Use inquirer to prompt users with questions
