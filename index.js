8625e41c17a74f03e58842d174d0fcb4426729ee
Chris MacGeorge <christopher.s.macgeorge@gmail.com>
2020-10-04 13:12:19 -0700

    updated index.js questions

--- index.js
+++ index.js
@@ -3,7 +3,54 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 
 // Create an Array of questions
-
+const questions = [
+    {
+        message: "What is your github username?",
+        name: "username",
+        type: "input"
+    },
+    {
+        message: "What is your email address?",
+        name: "email",
+        type: "input"
+    },
+    {
+        message: "What would you like to title this project?",
+        name: "title",
+        type: "input"
+    },
+    {
+        message: "Please provide a brief description",
+        name: "description",
+        type: "input"
+    },
+    {
+        message: "Are there any notes for installation of this project?",
+        name: "installation",
+        type: "input"
+    },
+    {
+        message: "how does the user run this program?",
+        name: "usage",
+        type: "input"
+    },
+    {
+        message: "are there any licenses for this project?",
+        name: "license",
+        type: "list",
+        choices: ["MIT", "ISC", "APACHE 2.O", "NONE"]
+    },
+    {
+        message: "Would you like to contribute to this project?",
+        name: "contribute",
+        type: "input"
+    },
+    {
+        message: "Is there a testing requirement for this app?",
+        name: "test",
+        type: "input"
+    }
+]
 
 // Write a README in a markdown file as a template
 
